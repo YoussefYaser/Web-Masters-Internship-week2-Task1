92c94168814adf61da151145235b2a954da9176e
YoussefYaser <youssefyasser549@gmail.com>
2024-11-23 22:19:05 +0200

    Landing page V1.3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const page = document.querySelector('.page');
 const nav = document.querySelector('nav');
 const navBarToggler = document.querySelector('.page nav .navbar-toggler');
+const subscribeInput = document.querySelector('.page footer .subscribe input');
 
 page.style.setProperty('padding-top', `${nav.clientHeight}px`);
+
+// handle navbar shadow with scroll
 
 window.addEventListener('scroll', function(){
     if(this.scrollY < 200)
@@ -18,7 +21,21 @@ navBarToggler.addEventListener('click', function(){
     else if(!window.scrollY){
         nav.style.cssText = `box-shadow: none;`
     }
+});
+
+//---------------------------------------------------------------------------------------------
+
+// subscribe input placeholder toggle
+
+subscribeInput.addEventListener('blur', function(){
+    if(this.value){
+        this.nextElementSibling.nextElementSibling.style.opacity = '0';
+    }
+    else{
+        this.nextElementSibling.nextElementSibling.style.opacity = '1';
+    }
 })
+
 
 
 
